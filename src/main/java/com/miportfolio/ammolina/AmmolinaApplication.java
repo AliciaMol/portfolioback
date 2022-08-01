2b59882bba3025a9f928c66d76058e9558fe74fa
AliciaMol <54749534+AliciaMol@users.noreply.github.com>
2022-07-31 22:23:58 -0300

    corrigiendo dominio y config de cors

--- src/main/java/com/miportfolio/ammolina/AmmolinaApplication.java
+++ src/main/java/com/miportfolio/ammolina/AmmolinaApplication.java
@@ -21,7 +21,7 @@ public class AmmolinaApplication {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliofront-18a58.web.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Accept", "Authorization", "Origin, Accept", "X-Request-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
                 "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
